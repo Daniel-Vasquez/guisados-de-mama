4c227093b1447f1c16d051055e06ae7c9e354fc3
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2024-04-09 17:57:05 -0600

    Install tailwind

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,7 @@
 import { LogoWhatsapp } from "@/components/icons/LogoWhatsapp";
 import contactImg from "@/images/contact-img.jpeg";
 const PUBLIC_MY_PHONE_NUMBER = import.meta.env.PUBLIC_MY_PHONE_NUMBER;
-const DEFAULT_TEXT = "Hola,%20Guisados%20de%20mamá,%20me%20gustaría%20ordenar%20un%20guisado,%20¿podrían%20ayudarme?";
+const DEFAULT_TEXT = "Hola,%20*Guisados%20de%20mamá*,%20me%20gustaría%20ordenar%20un%20guisado,%20¿podrían%20ayudarme?";
 
 export function Contact() {
   return (
