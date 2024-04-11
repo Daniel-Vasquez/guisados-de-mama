af7f47353ee113f399144e329fb619d28ce99fcc
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2024-04-11 09:18:26 -0600

    Changing Banner image

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -7,15 +7,15 @@ export function Contact() {
       className="flex justify-center items-center bg-cover bg-center min-h-screen"
       style={{ backgroundImage: `url(${contactImg.src})` }}
     >
-      <div className="bg-blue-light flex flex-col max-w-xl w-full mx-3 rounded-xl px-4 py-14">
+      <div className="bg-orange flex flex-col max-w-xl w-full mx-3 rounded-xl px-4 py-14">
         <div className="flex flex-col items-center justify-center w-full h-full">
-          <h1 className="mb-4 text-4xl font-bold text-white">Contactanos</h1>
-          <p className="mb-8 text-lg text-center text-white">
+          <h1 className="mb-4 text-5xl font-bold text-blue">Contactanos</h1>
+          <p className="mb-8 text-lg text-center text-blue">
             ¿Tegustaría ordenar un guisado? ¡Contáctanos!
           </p>
         </div>
 
-        <div className="bg-blue w-64 m-auto rounded-md hover:bg-blue-medium">
+        <div className="bg-blue w-64 m-auto rounded-md hover:bg-blue-light">
           <ContactButton 
             text="Via Whatsapp" 
             classButton="flex justify-center items-center gap-4 w-full h-24 text-white text-2xl font-bold"
